9d420aa1414441aa82942cd5c205a54dd7d25ac3
ddrsmiLe <ddrsmile@gmail.com>
2016-10-05 23:56:38 +0900

    using mezzanine

--- joeyliu/project/apps/joeyliu/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'project.apps.joeyliu'
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-]
--- joeyliu/project/wsgi.py
+++ joeyliu/project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project project.
+WSGI config for joeyliu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings.settings" % real_project_name("project"))
 
 application = get_wsgi_application()
