9ca3dc5a398e35b3621a2d61b76de313e8030b1a
ddrsmiLe <ddrsmile@gmail.com>
2016-11-16 23:09:55 +0900

    reset home model and change background image

--- joeyliu/home/models.py
+++ joeyliu/home/models.py
@@ -4,9 +4,6 @@ from django.db import models
 
 from modelcluster.fields import ParentalKey
 
-# customized widget models
-from widgets.models import CarouselItem
-
 # wagtail models
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailadmin.edit_handlers import (FieldPanel,
@@ -15,9 +12,5 @@ from wagtail.wagtailadmin.edit_handlers import (FieldPanel,
 class HomePage(Page):
     pass
 
-class HomePageCarouselItem(Orderable, CarouselItem):
-    page = ParentalKey('HomePage', related_name='carousel_items')
 
-HomePage.content_panels = Page.content_panels + [
-        InlinePanel('carousel_items', label="Carousel items"),
-]
+HomePage.content_panels = Page.content_panels
