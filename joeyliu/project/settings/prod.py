13e6bb64b8c5847ad651f14efae721f5fd2b46c0
ddrsmiLe <ddrsmile@gmail.com>
2016-11-17 01:16:26 +0900

    change static files path for production

--- joeyliu/project/settings/prod.py
+++ joeyliu/project/settings/prod.py
@@ -6,7 +6,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-# using s3 as the storage of static and media files
+# static path
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# using s3 as the storage for media files
 import csv
 # settings of aws authentication, bucket name and domain
 AWS_PEM = csv.reader(open('/usr/local/etc/aws_credentials.csv'), delimiter=',')
@@ -19,14 +23,14 @@ AWS_STORAGE_BUCKET_NAME = 'joeyliu-webapps'
 AWS_S3_CUSTOM_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
 # settings of project name and relatived static and media path
-STATICFILES_LOCATION = 'joeyliu/static'
+#STATICFILES_LOCATION = 'joeyliu/static'
 MEDIAFILES_LOCATION = 'joeyliu/media'
 
-STATIC_URL = "https://{0}/{1}/".format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+#STATIC_URL = "https://{0}/{1}/".format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIA_URL = "https://{0}/{1}/".format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 # tell boto the place where static files and media files are
-STATICFILES_STORAGE = 'project.settings.custom_path.StaticStorage'
+#STATICFILES_STORAGE = 'project.settings.custom_path.StaticStorage'
 DEFAULT_FILE_STORAGE = 'project.settings.custom_path.MediaStorage'
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
