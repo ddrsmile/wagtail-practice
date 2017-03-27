5eebf6ca7c4cee11ed6f71681ccf813763639daa
ddrsmiLe <ddrsmile@gmail.com>
2017-03-28 00:08:07 +0900

    rebuild the structure

--- src/project/settings/prod.py
+++ src/project/settings/prod.py
@@ -25,19 +25,16 @@ AWS_STORAGE_BUCKET_NAME = 'joeyliu-webapps'
 AWS_S3_CUSTOM_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
 # settings of project name and relatived static and media path
-#STATICFILES_LOCATION = 'joeyliu/static'
 MEDIAFILES_LOCATION = 'joeyliu/media'
 
-#STATIC_URL = "https://{0}/{1}/".format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIA_URL = "https://{0}/{1}/".format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 # tell boto the place where static files and media files are
-#STATICFILES_STORAGE = 'project.settings.custom_path.StaticStorage'
 DEFAULT_FILE_STORAGE = 'project.settings.custom_path.MediaStorage'
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://joeyliu.me'
+BASE_URL = 'https://joeyliu.me'
 
 try:
     from .local import *
