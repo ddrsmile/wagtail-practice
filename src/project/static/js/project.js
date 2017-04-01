bb4e05d33a9b83a58f12caadbd83cc84df6422ea
ddrsmiLe <ddrsmile@gmail.com>
2017-04-01 20:56:54 +0900

    updated page layout

--- src/project/static/js/project.js
+++ src/project/static/js/project.js
@@ -1,4 +1,17 @@
+
+
 $(document).ready(function(){
+    // hightlight active page
+    $(function(){
+        var matched = location.pathname.match(/([^/]*\/){2}/);
+        var current = matched? matched[0]:location.pathname;
+        $('.navbar-table td a').each(function () {
+            if ($(this).attr('href') == current) {
+                $(this).parent().addClass('td-active');
+            }
+        });
+    })
+
     $('.navbar-table td a').hover(function () {
         $(this).parent().toggleClass('td-hover');
     });
