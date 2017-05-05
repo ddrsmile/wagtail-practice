98c3abb6c3cef459f7144bf308cbc57d48069494
ddrsmiLe <ddrsmile@gmail.com>
2017-05-05 16:21:47 +0900

    changed code editor from codemirror to ace editor

--- src/parts/widgets.py
+++ src/parts/widgets.py
@@ -7,54 +7,57 @@ from wagtail.utils.widgets import WidgetWithScript
 
 class CodeTextWidget(WidgetWithScript, widgets.Textarea):
     def render_js_init(self, id_, name, value):
-        # default mode is set to python
-
         jsinit = """
+            if (window.ACEInstances == null) {{
+                window.ACEInstances = {{}};
+            }}
+            // get the information used to create ace editor for each block
             var code_value_id = "{id!s}"
             var mode_value_id = code_value_id.replace("code", "language");
             var code = document.getElementById(code_value_id);
-            var mode = document.getElementById(mode_value_id);
-
-            if (window.CodeMirrorInstances == null) {{
-                window.CodeMirrorInstances = {{}};
-            }}
-            cm = CodeMirror.fromTextArea(
-                code,
-                {{
-                    mode: mode.value,
-                    theme: "solarized light",
-                    lineNumbers: true,
-                    styleActiveLine: true,
-                    matchBrackets: true,
-                    indentUnit: 4,
-                    extraKeys: {{
-                        "Tab": function(cm){{
-                            cm.replaceSelection("    " , "end");
-                        }}
-                    }}
-                }}
-            )
-            window.CodeMirrorInstances[code_value_id] = cm;
+            var mode = document.getElementById(mode_value_id).value;
+            // mode_map is defined in aes_utils.jp
+            mode = mode_map[mode];
+            var code_panel = document.createElement("div");
+            code.parentElement.appendChild(code_panel);
+            code.style.display = "none";
+            
+            // create ace editor
+            var _editor = ace.edit(code_panel);
+            
+            // setup editor
+            _editor.$blockScrolling = Infinity;
+            _editor.container.style.height = "300px"
+            _editor.container.style.weight = "100%"
+            _editor.resize()
+            _editor.setTheme("ace/theme/chrome");
+            _editor.setShowPrintMargin(false);
+            _editor.setFontSize(13);
+            
+            // setup editor session
+            _editor.getSession().setUseWorker(false);
+            _editor.getSession().setUseSoftTabs(true);
+            _editor.getSession().setValue(code.value);
+            _editor.getSession().setMode("ace/mode/" + mode);
+            
+            // added event listener to update code textarea
+            // pass session into the function to bind the corresponding textarea.
+            _editor.getSession().on("change", function(e, _session) {{
+                var code = document.getElementById("{id!s}");
+                code.value = _session.getValue();
+            }});
+            
+            window.ACEInstances[code_value_id] = _editor;
         """
         return jsinit.format(id=id_)
     
     @property
     def media(self):
         js = [
-            static('parts/codemirror/lib/codemirror.js'),
-            static('parts/codemirror/lib/utils.js'),
-            static('parts/codemirror/mode/python.js'),
-            static('parts/codemirror/mode/clike.js'),
-            static('parts/codemirror/mode/javascript.js'),
-            static('parts/codemirror/mode/css.js'),
-            static('parts/codemirror/mode/shell.js'),
+            static('parts/ace/ace.js'),
+            static('parts/ace/utils.js'),
         ]
-        css = {
-            'all': [
-                static('parts/codemirror/lib/codemirror.css'),
-                static('parts/codemirror/theme/solarized.css'),
-        ]}
-        return Media(js=js, css=css)
+        return Media(js=js)
 
 class MarkDownWidget(WidgetWithScript, widgets.Textarea):
 
